52d2a11ea74e6fb9c17ca1a4065c6bb6835bcf57
David Fales <davidfalesct@gmail.com>
2023-05-30 16:52:09 -0600

    challenge

---
+++ clock/script.js
@@ -0,0 +1,5 @@
+console.log('this is your clock MAKE IT WORK')
+
+//create a working clock for seconds that change to minutes that change to hours
+//bonus points if you can make it all reset to 0 after 24 hours
+
